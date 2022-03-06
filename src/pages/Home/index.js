30561fd1931544b60c533891cd0a6c5159b67340
LucasLottiPzm <lucas.lotti@grupopzm.com>
2022-03-06 20:06:45 -0300

    Change index of home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,6 +2,7 @@ import {useState} from "react";
 import Navegador from "../../geral/navbar";
 import { useNavigate } from "react-router-dom";
 import {Form, Button, Container, Card} from "react-bootstrap"
+import { Divisao } from "./style";
 import "bootstrap/dist/css/bootstrap.min.css";
 import icon from "../../../src/lolicon.png";
 
@@ -17,22 +18,24 @@ export default function Home(){
     return (
         <Container md = "auto" >
             <Navegador/>
-            <Card>
-                <Form class = "mb-5">
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                        <Form.Label>Nickname:</Form.Label>
-                        <Form.Control type="text" placeholder="name" value={summoner} onChange={(e) => setSummoner(e.target.value)} />
-                    </Form.Group>
-                    <Button type="button" onClick={() => handleClick()} float="right">
-                        <img
-                        alt="Icone"
-                        src={icon}
-                        width="40"
-                        height="40"
-                        />
-                    </Button>
-                </Form>
-            </Card>
+            <Divisao>
+                <Card >
+                    <Form className = "mb-5" style={{ display:'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1" >
+                            <Form.Label>Nickname:</Form.Label>
+                            <Form.Control type="text" placeholder="name" value={summoner} onChange={(e) => setSummoner(e.target.value)} />
+                        </Form.Group>
+                        <Button type="button" onClick={() => handleClick()} float="right">
+                            <img
+                            alt="Icone"
+                            src={icon}
+                            width="40"
+                            height="40"
+                            />
+                        </Button>
+                    </Form>
+                </Card>
+            </Divisao>
         </Container>
     )
 }
---
+++ src/pages/Home/style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export const Divisao = styled.div`
+  padding-top: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
